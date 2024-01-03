0b25c370559cf2cbeae2b01b9d04ce6ca0e61ff5
Marcelo2507 <marceloluigi12.p8@gmail.com>
2024-01-03 11:47:59 -0300

    criando a base sem styled-components

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
-import { Text } from 'react-native';
-import {  View } from './app/src/styles/custom';
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { StyleSheet,Text, TouchableOpacity, View } from 'react-native';
+//import {  View, TextAdd, ButtonAdd } from './app/src/styles/custom';
 
 export default function App() {
+  const [number, setNumber] = useState(0)
+
+  function increment(){
+    return setNumber(number + 1);
+  }
+
+  
   return (
-    <View>
-      <Text>Ola mundo!</Text>
+    <View style={styles.container}>
+      <Text style={{ color: "white" }}>Ola mundo!</Text>
+      <StatusBar style="dark"/>
+
+      <TouchableOpacity onPress={increment} style={styles.button}>
+        <Text style={styles.text}>Add +</Text>
+      </TouchableOpacity>
+      
+    <Text style={styles.text}>{number}</Text>
 
     </View>
   );
 }
+
+const styles= StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#3c241b",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  text: {
+    color: "white",
+    fontSize: 20,
+    marginTop: 20,
+  },
+
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+  },
+});
--- app/src/styles/custom.js
+++ app/src/styles/custom.js
@@ -4,5 +4,16 @@ export const View = styled.SafeAreaView`
 flex: 1;
 align-items: center;
 justify-content: center;
-background-color: #fff;
+background-color: #000;
+`;
+
+export const ButtonAdd = styled.TouchbleOpacyt`
+  background-color: "#097379,
+  padding: 20,
+`;
+
+export const TextAdd = styled.Text`
+  color: white,
+  font-size: 20,
+  margin-top: 20,
 `;
